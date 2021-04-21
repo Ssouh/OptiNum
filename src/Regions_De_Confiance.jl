91eaa6066912fba742aeaf1b5c1ddb8ca4619bb1
Souhail AMG <46264437+Ssouh@users.noreply.github.com>
2021-04-21 15:09:16 +0200

    Update Regions_De_Confiance.jl

--- src/Regions_De_Confiance.jl
+++ src/Regions_De_Confiance.jl
@@ -1,4 +1,56 @@
-function Regions_De_Confiance(algo,f::Function,gradf::Function,hessf::Function,x0,options)
+@doc doc"""
+Minimise une fonction en utilisant l'algorithme des régions de confiance avec
+    - le pas de Cauchy
+ou
+    - le pas issu de l'algorithme du gradient conjugue tronqué
+
+# Syntaxe
+```julia
+xk, nb_iters, f(xk), flag = Regions_De_Confiance(algo,f,gradf,hessf,x0,option)
+```
+
+# Entrées :
+
+   * **algo**        : (String) string indicant la méthode à utiliser pour calculer le pas
+        - **"gct"**   : pour l'algorithme du gradient conjugué tronqué
+        - **"cauchy"**: pour le pas de Cauchy
+   * **f**           : (Function) la fonction à minimiser
+   * **gradf**       : (Function) le gradient de la fonction f
+   * **hessf**       : (Function) la hessiene de la fonction à minimiser
+   * **x0**          : (Array{Float,1}) point de départ
+   * **options**     : (Array{Float,1})
+     * **deltaMax**      : utile pour les m-à-j de la région de confiance
+                      ``R_{k}=\left\{x_{k}+s ;\|s\| \leq \Delta_{k}\right\}``
+     * **gamma1,gamma2** : ``0 < \gamma_{1} < 1 < \gamma_{2}`` pour les m-à-j de ``R_{k}``
+     * **eta1,eta2**     : ``0 < \eta_{1} < \eta_{2} < 1`` pour les m-à-j de ``R_{k}``
+     * **delta0**        : le rayon de départ de la région de confiance
+     * **max_iter**      : le nombre maximale d'iterations
+     * **Tol_abs**       : la tolérence absolue
+     * **Tol_rel**       : la tolérence relative
+
+# Sorties:
+
+   * **xmin**    : (Array{Float,1}) une approximation de la solution du problème : ``min_{x \in \mathbb{R}^{n}} f(x)``
+   * **fxmin**   : (Float) ``f(x_{min})``
+   * **flag**    : (Integer) un entier indiquant le critère sur lequel le programme à arrêter
+      - **0**    : Convergence
+      - **1**    : stagnation du ``x``
+      - **2**    : stagnation du ``f``
+      - **3**    : nombre maximal d'itération dépassé
+   * **nb_iters** : (Integer)le nombre d'iteration qu'à fait le programme
+
+# Exemple d'appel
+```julia
+algo="gct"
+f(x)=100*(x[2]-x[1]^2)^2+(1-x[1])^2
+gradf(x)=[-400*x[1]*(x[2]-x[1]^2)-2*(1-x[1]) ; 200*(x[2]-x[1]^2)]
+hessf(x)=[-400*(x[2]-3*x[1]^2)+2  -400*x[1];-400*x[1]  200]
+x0 = [1; 0]
+options = []
+xmin, fxmin, flag,nb_iters = Regions_De_Confiance(algo,f,gradf,hessf,x0,options)
+```
+"""
+function Regions_De_Confiance(algo, f::Function, gradf::Function, hessf::Function, x0, options)
 
     if options == []
         deltaMax = 10
@@ -7,7 +59,7 @@ function Regions_De_Confiance(algo,f::Function,gradf::Function,hessf::Function,x
         eta1 = 0.25
         eta2 = 0.75
         delta0 = 2
-        max_iter = 1000
+        max_iter = 30000
         Tol_abs = sqrt(eps())
         Tol_rel = 1e-15
     else
@@ -23,58 +75,56 @@ function Regions_De_Confiance(algo,f::Function,gradf::Function,hessf::Function,x
     end
 
     n = length(x0)
-    xmin = zeros(n)
-    fxmin = f(xmin)
-    flag = 10
     nb_iters = 0
-    
-    deltak = delta0
     xk = x0
-    xk2 = xk
-    fk0 = f(xk)
-    
-    while(flag == 10)
-        xk = xk2
-        gk = gradf(xk)
-        hk = hessf(xk)
-        if algo == "cauchy"
-            sk, ek = Pas_De_Cauchy(gk, hk, deltak)
-        elseif algo == "gct"
-            options = [deltak, max_iter, tol_abs]
-            sk = Gradient_Conjugue_Tronque(fk,gradfk,hessfk,option)
-        else return "Erreur : Methode de calcul pas inconnue"
-        end
-            
-        fk = f(xk+sk)
-        rhok = (fk0-fk)/(-gk'*sk - 0.5*sk'*hk*sk)
-        
-        if (rhok >= eta1)
-            xk2 = xk + sk
-            fk0 = fk
-        end
-        
-        if rhok >= eta2
-            deltak = min(gamma2*deltak, deltaMax)
-        elseif rhok < eta1
-            deltak = gamma1 * deltak
-        end
-        
-        # Tests de convergence
-        if norm(gradf(xk2)) <= max(Tol_rel*norm(gradf(x0)),Tol_abs) "test de Convergence flag--> 0 "
-            flag = 0
-        elseif norm(xk2-xk) <= max(Tol_rel*norm(xk),Tol_abs)  "test de stagnation de xk flag--> 1 "
-            flag = 1
-        elseif abs(f(xk2) - f(xk)) <= max(Tol_rel*abs(f(xk)),Tol_abs)  "test de stagnation de f(xk) flag--> 2 "
-            flag = 2
-        elseif max_iter <= nb_iters  "test sur le nombre d'itération maximal  flag--> 3 "
-            flag = 3
-        end
-        
-        nb_iters = nb_iters+1
-    end
-    
-    xmin = xk
-    fxmin = f(xmin)
+    xk_1 = x0
+    delta = delta0
 
-    return xmin, fxmin, flag, nb_iters
+    for i in 1 : max_iter
+        nb_iters = i
+        xk = xk_1
+        
+        if algo == "gct"
+            sk = Gradient_Conjugue_Tronque(gradf(xk), hessf(xk), [delta , 3000, Tol_rel])
+        elseif algo == "cauchy"
+            sk, ~ = Pas_De_Cauchy(gradf(xk), hessf(xk), delta)
+        end
+
+        q(s) = f(xk) + gradf(xk)' * (s-xk) + (1 / 2) * (s - xk)' * hessf(xk) * (s - xk)
+        f_xk_sk = f(xk + sk)
+        rho_k = (f(xk) - f(xk + sk)) / (q(xk) - q(xk + sk))
+        if rho_k >= eta1
+            xk_1 = xk + sk
+        else
+            xk_1 = xk
+        end
+
+        if rho_k >= eta2
+            delta = min(gamma2 * delta, deltaMax)
+        elseif rho_k >= eta1
+            delta = delta
+        else
+            delta = gamma1 * delta
+        end
+
+        if norm(gradf(xk_1)) < max(Tol_abs, Tol_rel * norm(gradf(x0)))
+            flag = 0
+            break
+        elseif norm(sk) < max(Tol_abs, Tol_rel * norm(xk))
+            flag = 1
+            break
+        elseif abs(f(xk + sk) - f(xk)) < max(Tol_abs, Tol_rel * abs(f(xk)))
+            flag = 2
+            break
+        end
+    end
+
+    if nb_iters == max_iter
+        flag = 3
+    end
+
+    xmin = xk_1
+    f_min = f(xmin)
+
+    return xmin, f_min, flag, nb_iters
 end
